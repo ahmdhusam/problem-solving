f86c8490d68598347a47e2b6ab75379457132cac
ahmdhusam <ahmdhusam7@gmail.com>
2022-05-06 03:46:54 +0200

    init

---
+++ src/Hard/4.Median-of-Two-Sorted-Arrays.ts
@@ -0,0 +1,10 @@
+// function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+//     const nums = [...nums1].concat([...nums2]).sort((a, b) => a - b);
+//     const mid = Math.floor((nums.length - 1) / 2);
+
+//     if (nums.length % 2 === 0) {
+//         return (nums[mid] + nums[mid + 1]) / 2;
+//     } else {
+//         return nums[mid];
+//     }
+// }
